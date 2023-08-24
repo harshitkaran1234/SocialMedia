6c6105880c1b4424b9318785dd8833ad66ad66f5
karanharshit <harsheet2020@gmail.com>
2023-08-24 10:46:54 +0530

    database connection made using mongodb, mongoose

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { checkLogin } = require('./middleware');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://harsheet2020:NarutoLuffy@cluster0.kh572kx.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    console.log('database connection successful');
+})
+.catch((err) => {
+    console.log(`database connection error ${err}`);
+})
 
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
-const { checkLogin } = require('./middleware');
 
 const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoute');
